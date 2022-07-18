e86e5509f00432fa80b6f7f5d9ac8cce7fec1f44
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 20:01:12 +0700

    tugas 22 js
    untuk niomic

---
+++ Tugas22/Tugas22.js
@@ -0,0 +1,8 @@
+function panggilforEach() {
+  var data = ['saya Index ke','ingin Index ke','belajar Index ke','bersama Index ke'];
+  data.forEach(function (item, index){
+  console.log(item, index);
+
+})
+}
+panggilforEach()
